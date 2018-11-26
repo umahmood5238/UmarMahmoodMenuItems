b7e9467eddae352e070a73340aafa241beb99507
BT_1N3_29@bktech.local <BT_1N3_29@bktech.local>
2018-11-26 08:41:46 -0500

    Initial commit

---
+++ src/com/company/MenuRunner.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class MenuRunner {
+    public static void main (String[] args) {
+        Sandwich Reuben = new Sandwich (5.75, "Reuben");
+        Sandwich bec = new Sandwich (2.00, "BEC");
+        Sandwich Waldorf = new Sandwich (7.25, "Waldorf");
+        Sandwich Caeser = new Sandwich (4.50, "Caeser");
+        Sandwich icedTea = new Sandwich (1.50, "Iced Tea");
+        Sandwich Smoothie = new Sandwich (5.50, "Smoothie");
+
+        Trio trio1 = new Trio ()
+    }
+}
